4a7b6788f6d1ff4c32d372b5d665b8d41ba68a0e
brenden.dielissen <brenden.dielissen@ucalgary.ca>
2021-05-04 12:22:35 -0600

    Adding git ignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='quote-gui',
+    version='1.0.0',
+    packages=['quote_gui'],
+    url='https://github.com/brendoser/quote-gui',
+    license='',
+    author='brenden.dielissen',
+    author_email='brendendielissen@gmail.com',
+    description='A GUI for displaying quotes from books on Amazon.'
+)
